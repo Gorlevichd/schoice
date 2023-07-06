7938703e56dee7b06db800a6f514c82c4d3304ae
Gorlevichd <gorlevichd@gmail.com>
2023-07-06 17:43:03 +0300

    Added tests, updated docs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Small Python project for Social Choice calculations
 
-Consists of one big class: Social_Choice
+Consists of one big class:
 
 For now, the package works only with tasks that are formulated as follows:
 
@@ -13,4 +13,12 @@ For now, the package works only with tasks that are formulated as follows:
 
 Social choice class takes 2 dimensional list of rankings as in table and corresponding numbers of voters (first row of the table)
 
-For use cases see Example.md
+For use cases see docs/Docs.md
+
+## Installation 
+
+To install package run:
+
+`pip install git+https://github.com/Gorlevichd/schoice.git`
+
+Package requires `numpy`.
--- tests/test_aggr.py
+++ tests/test_aggr.py
@@ -57,19 +57,19 @@ def test1(matrix1):
 
     ## Copeland
     copeland_result = copeland_rule(matrix1)
-    assert copeland_result[0] == "a"
+    assert copeland_result[0].item() == "a"
     assert np.array_equal(copeland_result[1], np.array(["a", "b", "c"]))
     assert np.array_equal(copeland_result[2], np.array([2, 0, -2]))
 
     ## Simpson
     simpson_result = simpson_rule(matrix1)
-    assert simpson_result[0] == "a"
+    assert simpson_result[0].item() == "a"
     assert np.array_equal(simpson_result[1], np.array(["a", "b", "c"]))
     assert np.array_equal(simpson_result[2], np.array([9, 8, 3]))
 
     ## Borda
     borda_result = scoring_rule(matrix1)
-    assert borda_result[0] == "b"
+    assert borda_result[0].item() == "b"
     assert np.array_equal(borda_result[1], np.array(["a", "b", "c"]))
     assert np.array_equal(borda_result[2], np.array([19, 22, 10]))
 
@@ -86,3 +86,102 @@ def test1(matrix1):
     assert runoff_result[1].item() == "a"
     assert np.array_equal(runoff_result[2], np.array(["a", "b"]))
     assert np.array_equal(runoff_result[3], np.array([9, 8]))
+
+
+def test2(matrix2):
+
+    ## Condorcet
+    condorcet_result = condorcet_rule(matrix2)
+    assert np.size(condorcet_result[0]) == 0
+    assert np.array_equal(condorcet_result[1], np.array(["a", "b", "c", "d", "e"]))
+    assert np.array_equal(condorcet_result[2], np.array([
+        [1, 1, 1, 1, -1],
+        [-1, 1, 1, -1, 1],
+        [-1, -1, 1, 1, 1],
+        [-1, 1, -1, 1, 1],
+        [1, -1, - 1, -1, 1]
+    ]))
+
+    ## Copeland
+    copeland_result = copeland_rule(matrix2)
+    assert copeland_result[0].item() == "a"
+    assert np.array_equal(copeland_result[1], np.array(["a", "b", "c", "d", "e"]))
+    assert np.array_equal(copeland_result[2], np.array([2, 0, 0, 0, -2]))
+
+    ## Simpson
+    simpson_result = simpson_rule(matrix2)
+    assert np.array_equal(simpson_result[0], ["b", "c", "d", "e"])
+    assert np.array_equal(simpson_result[1], np.array(["a", "b", "c", "d", "e"]))
+    assert np.array_equal(simpson_result[2], np.array([1, 4, 4, 4, 4]))
+
+    ## Borda
+    borda_result = scoring_rule(matrix2)
+    assert borda_result[0].item() == "e"
+    assert np.array_equal(borda_result[1], np.array(["a", "b", "c", "d", "e"]))
+    assert np.array_equal(borda_result[2], np.array([16, 18, 18, 18, 20]))
+
+    ## Scoring, custom weights
+    scoring_result = scoring_rule(matrix2, weights = [6, 7, 8, 9, 10])
+    assert scoring_result[0].item() == "e"
+    assert np.array_equal(scoring_result[1], np.array(["a", "b", "c", "d", "e"]))
+    assert np.array_equal(scoring_result[2], np.array([70, 72, 72, 72, 74]))
+
+    ## Plurality
+    plurality_result = plurality_rule(matrix2)
+    assert plurality_result[0] == 1
+    assert np.array_equal(plurality_result[1], np.array(["c", "e"]))
+    assert np.array_equal(plurality_result[2], np.array(["a", "b", "c", "d", "e"]))
+    assert np.array_equal(plurality_result[3], np.array([1, 0, 4, 0, 4]))
+
+    # Runoff
+    runoff_result = plurality_rule(matrix2, runoff = True)
+    assert runoff_result[0] == 2
+    assert runoff_result[1].item() == "c"
+    assert np.array_equal(runoff_result[2], np.array(["c", "e"]))
+    assert np.array_equal(runoff_result[3], np.array([5, 4]))
+
+
+def test3(matrix3):
+
+    ## Condorcet
+    condorcet_result = condorcet_rule(matrix3)
+    assert condorcet_result[0].item() == "c"
+    assert np.array_equal(condorcet_result[1], np.array(["a", "b", "c", "d"]))
+    assert np.array_equal(condorcet_result[2], np.array([
+        [1, -1, -1, -1],
+        [1, 1, -1, -1],
+        [1, 1, 1, 1],
+        [1, 1, -1, 1]
+    ]))
+
+    ## Copeland
+    copeland_result = copeland_rule(matrix3)
+    assert copeland_result[0].item() == "c"
+    assert np.array_equal(copeland_result[1], np.array(["a", "b", "c", "d"]))
+    assert np.array_equal(copeland_result[2], np.array([-3, -1, 3, 1]))
+
+    ## Simpson
+    simpson_result = simpson_rule(matrix3)
+    assert simpson_result[0].item() == "c"
+    assert np.array_equal(simpson_result[1], np.array(["a", "b", "c", "d"]))
+    assert np.array_equal(simpson_result[2], np.array([8, 5, 9, 8]))
+
+    ## Borda
+    borda_result = scoring_rule(matrix3)
+    assert borda_result[0].item() == "d"
+    assert np.array_equal(borda_result[1], np.array(["a", "b", "c", "d"]))
+    assert np.array_equal(borda_result[2], np.array([24, 22, 27, 29]))
+
+    ## Plurality
+    plurality_result = plurality_rule(matrix3)
+    assert plurality_result[0] == 1
+    assert plurality_result[1].item() == "a"
+    assert np.array_equal(plurality_result[2], np.array(["a", "b", "c", "d"]))
+    assert np.array_equal(plurality_result[3], np.array([8, 5, 4, 0]))
+
+    ## Runoff 
+    runoff_result = plurality_rule(matrix3, runoff = True)
+    assert runoff_result[0] == 2
+    assert runoff_result[1].item() == "b"
+    assert np.array_equal(runoff_result[2], np.array(["a", "b"]))
+    assert np.array_equal(runoff_result[3], np.array([8, 9]))
